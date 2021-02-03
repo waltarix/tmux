094b9e1575bb50f7fc6737732213914d4f99a44f
waltari <gitting.crew@gmail.com>
2021-02-03 17:21:19 +0900

    custom

--- autogen.sh
+++ autogen.sh
@@ -11,6 +11,25 @@ die()
     exit 1
 }
 
+has_command()
+{
+	command -v "$1" > /dev/null
+}
+
+http_download()
+{
+	if has_command curl; then
+		curl -sL "$@"
+	elif has_command wget; then
+		wget -q -O - "$@"
+	else
+		echo "curl or wget is not found" 1>&2
+		return 1
+	fi
+}
+
+http_download "https://github.com/waltarix/localedata/releases/download/13.0.0-r1/wcwidth9.h" > wcwidth9.h
+
 mkdir -p etc
 aclocal || die "aclocal failed"
 automake --add-missing --force-missing --copy --foreign || die "automake failed"
